d9b4c47e2c6c733fd61de245126f9228951c8d16
PrzemekBarczyk <przemek.barczyk@gmail.com>
2021-12-09 11:31:59 +0100

    Sort list of captured pieces

--- Assets/Scripts/UI/CapturedPieces.cs
+++ Assets/Scripts/UI/CapturedPieces.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +10,8 @@ public class CapturedPieces : MonoBehaviour
 	[SerializeField] Image _pieceIconPrefab;
 
 	[SerializeField] PiecesSprites _piecesSprites;
+
+	List<Tuple<PieceType, Image>> _capturedPiecesIcons = new List<Tuple<PieceType, Image>>();
 
 	Vector2 _currentIconPosition;
 	Vector2 _offset = new Vector2(-30, 0);
@@ -20,8 +24,35 @@ public class CapturedPieces : MonoBehaviour
 	public void AddCaptureIcon(PieceType pieceType)
 	{
 		Image newPieceIcon = Instantiate(_pieceIconPrefab, _currentIconPosition, transform.rotation, transform);
+
 		newPieceIcon.sprite = _piecesSprites.GetSprite(_color, pieceType);
 		newPieceIcon.SetNativeSize();
+
 		_currentIconPosition += _offset;
+
+		_capturedPiecesIcons.Add(new Tuple<PieceType, Image>(pieceType, newPieceIcon));
+
+		SortIcons();
+	}
+
+	void SortIcons()
+	{
+		for (int i = 0; i < _capturedPiecesIcons.Count; i++)
+		{
+			for (int j = 0; j < _capturedPiecesIcons.Count - 1; j++)
+			{
+				if ((int)_capturedPiecesIcons[j + 1].Item1 > (int)_capturedPiecesIcons[j].Item1)
+				{
+					var positionTemp = _capturedPiecesIcons[j + 1].Item2.rectTransform.localPosition;
+					var listTemp = _capturedPiecesIcons[j + 1];
+
+					_capturedPiecesIcons[j + 1].Item2.rectTransform.localPosition = _capturedPiecesIcons[j].Item2.rectTransform.localPosition;
+					_capturedPiecesIcons[j + 1] = _capturedPiecesIcons[j];
+
+					_capturedPiecesIcons[j].Item2.rectTransform.localPosition = positionTemp;
+					_capturedPiecesIcons[j] = listTemp;
+				}
+			}
+		}
 	}
 }
